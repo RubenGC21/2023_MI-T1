bcee1e1635538784ddb295cf21e5308fa76258d2
RubenGC21 <112138593+RubenGC21@users.noreply.github.com>
2023-05-13 19:52:48 -0600

    Update exercise.py

--- assignments/1_OperacionesBasicas/src/exercise.py
+++ assignments/1_OperacionesBasicas/src/exercise.py
@@ -1,6 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+
+    numero1 = int(input("Dame un número: ")
+    numero2 = int(input("Dame un número: ")
+                  
+    suma = numero1 + numero2
+    resta = numero1 - numero2
+    multiplicacion = numero1 * numero2              
+    
+    print ("Suma: ", suma)
+    print ("Resta", resta)
+    print ("Multiplicacion", multiplicacion)              
 
 
 if __name__ == '__main__':
